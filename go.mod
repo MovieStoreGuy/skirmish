98fa71bfdb07f383281ad7fdb7dd85b941fa6435
Sean Marciniak <smarciniak@atlassian.com>
2019-04-11 14:50:01 +1000

    Updating types

--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	google.golang.org/api v0.3.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2
+	syreclabs.com/go/faker v1.0.1 // indirect
 )
--- pkg/types/plans.go
+++ pkg/types/plans.go
@@ -16,19 +16,21 @@ type Plan struct {
 }
 
 type Step struct {
-	Name        string   `json:"name" yaml:"name"`
-	Description string   `json:"description" yaml:"description"`
-	Operations  []string `json:"operations" yaml:"operations"`
-	Projects    []string `json:"projects" yaml:"projects"`
-	Exclude     struct {
-		Labels    map[string]string `json:"labels" yaml:"labels" description:"define the labels to ignore resource "`
-		Zones     []string          `json:"zones" yaml:"zones" description:"define the zones to ignore"`
-		Regions   []string          `json:"regions" yaml:"regions" description:"define the regions to ignore"`
-		Wildcards []string          `json:"wildcards" yaml:"wildcards" description:"If the affected resources doesn't match, see if its name matches the wildcard'"`
-	} `json:"exclude" yaml:"exclude" description:"define all the things to exclude on"`
-	Settings Settings      `json:"settings" yaml:"settings"`
-	Wait     time.Duration `json:"wait" yaml:"wait"`
-	Sample   float32       `json:"sample" yaml:"sample" description:"Sample is rate [0.0,100.0] that will determine the likely hood of an instance being affected"`
+	Name        string        `json:"name" yaml:"name"`
+	Description string        `json:"description" yaml:"description"`
+	Operations  []string      `json:"operations" yaml:"operations" description:"It is the name of the loaded minions in the orchestrator"`
+	Projects    []string      `json:"projects" yaml:"projects"`
+	Exclude     Exclude       `json:"exclude" yaml:"exclude" description:"define all the things to exclude on"`
+	Settings    Settings      `json:"settings" yaml:"settings"`
+	Wait        time.Duration `json:"wait" yaml:"wait"`
+	Sample      float32       `json:"sample" yaml:"sample" description:"Sample is rate [0.0,100.0] that will determine the likely hood of an instance being affected"`
+}
+
+type Exclude struct {
+	Labels    map[string]string `json:"labels" yaml:"labels" description:"define the labels to ignore resource "`
+	Zones     []string          `json:"zones" yaml:"zones" description:"define the zones to ignore"`
+	Regions   []string          `json:"regions" yaml:"regions" description:"define the regions to ignore"`
+	Wildcards []string          `json:"wildcards" yaml:"wildcards" description:"If the affected resources doesn't match, see if its name matches the wildcard'"`
 }
 
 type Settings struct {
@@ -45,8 +47,7 @@ type Deny struct {
 }
 
 // Validate will ensure that the expected format of the plan
-// is kept straight
-func (p *Plan) Validate() error {
+func (p *Plan) validate() error {
 	switch p.Mode {
 	case DryRun, Repairable, Destruction:
 		// Valid options
@@ -98,5 +99,5 @@ func LoadPlan(filepath string) (*Plan, error) {
 		}
 		step.Sample = step.Sample / 100.0
 	}
-	return &p, (&p).Validate()
+	return &p, (&p).validate()
 }
